3de5ba108d4fd79a8f3b39dc125793384f18eaf7
AshwinRamakrishnan <156045037+AshwinRamakrishnan@users.noreply.github.com>
2026-09-11 12:59:18 +0530

    Update geminiClient.js

--- src/api/geminiClient.js
+++ src/api/geminiClient.js
@@ -1,7 +1,7 @@
 // File: src/api/geminiClient.js
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyDFF0qlHYRFm0U9wKX5iLfuuMKfnvTw1Qs");
+const genAI = new GoogleGenerativeAI("");
 
 export const runGemini = async (inputText) => {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
