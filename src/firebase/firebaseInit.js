4dd38fbcc2ca801d08402b85051525c48c02b833
AshwinRamakrishnan <156045037+AshwinRamakrishnan@users.noreply.github.com>
2026-09-11 13:01:34 +0530

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -3,14 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-  apiKey: "AIzaSyDMj45IV8LXuOArD2DgtwvfB841dzcn620",
-  authDomain: "tn-futecx.firebaseapp.com",
-  databaseURL: "https://tn-futecx-default-rtdb.firebaseio.com",
-  projectId: "tn-futecx",
-  storageBucket: "tn-futecx-files.appspot.com", // ✅ spelling fix
-  messagingSenderId: "884450104101",
-  appId: "1:884450104101:web:eae68a54cdee6078f300cc",
-  measurementId: "G-TNYE4ZSD3C"
+
 };
 
 const app = initializeApp(firebaseConfig);
